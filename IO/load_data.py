363c163caf963cb8408cde90b28c55f7838ee3f7
yzerlaut <yann.zerlaut@gmail.com>
2017-03-03 15:13:24 +0100

    starting to clean up file organization and path

--- IO/load_data.py
+++ IO/load_data.py
@@ -1,33 +1,33 @@
 import numpy as np
-import sys
-sys.path.append('../../')
-import data_analysis.IO.binary_to_python as binary_to_python
+import sys, pathlib
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[2]))
+import data_analysis.IO.axon_to_python as axon
+import data_analysis.IO.binary_to_python as binary
 
-def get_formated_data(filename, zoom=[0,np.inf], with_params_only=False):
-    data = get_metadata(filename)
+def load_file(filename, zoom=[0,np.inf]):
     if filename.endswith('.bin'):
-        t, DD = binary_to_python.load_file(filename, zoom=zoom)
-    # if filename.endswith('.abf'):
-    #     t, DD = axon_to_python.load_file(filename, zoom=zoom)
+        return binary.load_file(filename, zoom=zoom)
+    elif filename.endswith('.abf'):
+        return axon.load_file(filename, zoom=zoom)
     else:
-        t, DD = 0, [[0]]
-    data['t'] = t
-    data['Vm'], data['Iinj'], data['Laser'], data['UP_FLAG'], data['Vm_LP'], data['CMD'] = DD[0], DD[1], DD[2], DD[3], DD[4], DD[5]
-    data['dt'] = (t[1]-t[0]) # data['dt'] in ms (transition times are stored in ms)
-    if with_params_only:
-        return data, get_metadata(filename)
-    else:
-        return data
+        return None
 
 def get_metadata(filename):
+    print('filename is', filename)
     if filename.endswith('.bin'):
-        return binary_to_python.get_metadata(filename)
-    # if filename.endswith('.abf'):
-    #     return axon_to_python.get_metadata(filename)
-    if filename.endswith('.npz'):
+        return binary.get_metadata(filename)
+    elif filename.endswith('.abf'):
+        return axon.get_metadata(filename)
+    elif filename.endswith('.npz'):
         return {'main_protocol':'modeling_work'}
     else:
-        return {}
+        return None
 
-
-    
+if __name__ == '__main__':
+    import sys
+    import matplotlib.pylab as plt
+    filename = sys.argv[-1]
+    print(get_metadata(filename))
+    t, data = load_file(filename, zoom=[-5.,np.inf])
+    plt.plot(t[10000:], data[0][10000:])
+    plt.show()
