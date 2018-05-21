b421c0184c7db31ff59f91e7df3ec0f9ae1a9dd0
yzerlaut <yann.zerlaut@gmail.com>
2018-05-21 15:25:02 +0200

    üp on waveforms

--- signal_library/stim_waveforms.py
+++ signal_library/stim_waveforms.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-def double_exponential(amp, t0, T_rise, T_decay, dt=0.1, width_extent_factor=6.):
+def double_exponential(amp, t0, T_rise, T_decay,
+                       dt=0.1, width_extent_factor=6.,
+                       with_t=False):
     """
     You need to be careful with the parameters
 
@@ -14,4 +16,8 @@ def double_exponential(amp, t0, T_rise, T_decay, dt=0.1, width_extent_factor=6.)
     output = (1.-np.exp(-tt/T_rise))*np.exp(-tt/T_decay)
     # get the normalization factor analytically:
     factor = (1.-T_rise/(T_rise+T_decay))*np.power(T_rise/(T_rise+T_decay), T_rise/T_decay)
-    return output/factor*amp
+    output *= amp/factor
+    if with_t:
+        return tt, output
+    else:
+        return output
