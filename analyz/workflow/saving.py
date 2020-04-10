8a3e7b0e963508bcf0bf66acc809e6e777062d61
Yann <yann.zerlaut@icm-institute.org>
2020-04-10 15:45:48 +0200

    update filesaving

--- analyz/workflow/saving.py
+++ analyz/workflow/saving.py
@@ -1,4 +1,14 @@
 import time, os
 
 def filename_with_datetime(filename, folder='./', extension=''):
-    return os.path.join(folder,  filename+'__'+time.strftime("%Y_%m_%d-%H:%M:%S")+extension)
+    if extension.startswith('.'):
+        extension=extension[1:]
+    if filename!='':
+        filename=filename+'_'
+    return os.path.join(folder, filename, time.strftime("%Y_%m_%d-%H:%M:%S")+'.'+extension)
+
+
+if __name__=='__main__':
+
+    print(filename_with_datetime('', folder='./', extension='.npy'))
+    print(filename_with_datetime('', folder='./', extension='npy'))    
