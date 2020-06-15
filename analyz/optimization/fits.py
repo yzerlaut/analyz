f840ad2b56f3247e4d330285e318b6496ce79a1c
yzerlaut <yann.zerlaut@gmail.com>
2020-06-15 10:56:52 +0200

    adding a general func module in workflow

--- analyz/optimization/fits.py
+++ analyz/optimization/fits.py
@@ -1,14 +1,14 @@
 # Filename: fits.py
 
 import numpy as np
-from scipy.optimize import leastsq
+from scipy.optimize import leastsq, minimize
 
 def leastsq_fit(x, data, waveform, coeffs, verbose=False):
     """
     'waveform' has to be a fnction waveform(x, coeffs) :
     """
     def residuals(coeffs, y, x):
-        return np.abs(y - waveform(x, coeffs))
+        return np.abs(y - waveform(x, coeffs)).sum()
     
     C, flag = leastsq(residuals, coeffs, args=(data, x))
 
@@ -16,3 +16,33 @@ def leastsq_fit(x, data, waveform, coeffs, verbose=False):
         print(flag)
     return C
 
+def curve_fit(x, y, waveform, coeffs, bounds=None, verbose=False):
+    """
+    'waveform' has to be a fnction waveform(x, coeffs) :
+    """
+    def residual(coeff):
+        return np.abs(y - waveform(x, coeffs)).sum()
+    
+    res = minimize(residual, coeffs, bounds=bounds)
+
+    if verbose:
+        print(res)
+    return res
+
+
+if __name__=='__main__':
+
+
+    import numpy as np
+    from datavyz import ge
+
+    def waveform(x, coeffs=[.3, .2, 2.]):
+        return coeffs[2]*(np.sign(x-coeffs[0])+1.)*np.exp(-(x-coeffs[0])/coeffs[1])
+
+    import inspect
+    print(inspect.signature(waveform).coeffs)
+    # x = np.linspace(0, 1, 100)
+    # y = np.random.randn(100)+waveform(x)
+
+    # ge.plot(x, y)
+    # ge.show()
