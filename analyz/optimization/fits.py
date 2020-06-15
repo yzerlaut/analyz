21a37230df6f71905c9b3ee9b7cef31f61f71070
yzerlaut <yann.zerlaut@gmail.com>
2020-06-15 14:19:40 +0200

    done with the curve_fit template

--- analyz/optimization/fits.py
+++ analyz/optimization/fits.py
@@ -2,33 +2,90 @@
 
 import numpy as np
 from scipy.optimize import leastsq, minimize
+from analyz.workflow.funcs import get_default_args
 
-def leastsq_fit(x, data, waveform, coeffs, verbose=False):
+def leastsq_fit(x, y, waveform, coeffs=None, verbose=False):
     """
     'waveform' has to be a fnction waveform(x, coeffs) :
     """
-    def residuals(coeffs, y, x):
-        return np.abs(y - waveform(x, coeffs)).sum()
+    if coeffs is None:
+        try:
+            coeffs = get_default_args(waveform)['coeffs']
+        except NameError:
+            print('/!\ No default "coeffs" found in function, need to pass it explicitely !')
+
+            
+    def residual(c):
+        return (y - waveform(x, coeffs=c))
     
-    C, flag = leastsq(residuals, coeffs, args=(data, x))
+    C, flag = leastsq(residual, [.3, .2, 2.])
 
     if verbose:
         print(flag)
     return C
 
-def curve_fit(x, y, waveform, coeffs, bounds=None, verbose=False):
+def curve_fit(x, y, waveform, coeffs=None,
+              bounds=None,
+              method=None,
+              options={'maxiter':10000},
+              verbose=False):
     """
     'waveform' has to be a fnction waveform(x, coeffs) :
     """
-    def residual(coeff):
-        return np.abs(y - waveform(x, coeffs)).sum()
-    
-    res = minimize(residual, coeffs, bounds=bounds)
+    if coeffs is None:
+        try:
+            coeffs = get_default_args(waveform)['coeffs']
+        except NameError:
+            print('/!\ No default "coeffs" found in function, need to pass it explicitely !')
+            
+    def residual(c):
+        return np.sum((y-waveform(x, c))**2)/len(y)
 
     if verbose:
+        options['disp']=True
+
+    if (method is None) and (bounds is not None):
+        res = None
+    else:
+        res = minimize(residual, coeffs,
+                       method='Nelder-Mead',
+                       options=options)
+    if verbose:
         print(res)
+        
     return res
 
+def curve_fit(x, y, waveform, coeffs=None,
+              bounds=None,
+              method=None,
+              options={'maxiter':10000},
+              verbose=False):
+    # """
+    # 'waveform' has to be a fnction waveform(x, coeffs) :
+    # """
+    if coeffs is None:
+        try:
+            coeffs = get_default_args(waveform)['coeffs']
+        except NameError:
+            print('/!\ No default "coeffs" found in function, need to pass it explicitely !')
+
+    def to_minimize(c):
+        return np.sum((y-waveform(x, c))**2)/len(y)
+
+    if (method is None) and (bounds is None):
+        method='Nelder-Mead' # default method
+
+    if (bounds is not None):
+        res = minimize(to_minimize, coeffs,
+                       method=method, bounds=bounds, options=options)
+    else:
+        res = minimize(to_minimize, coeffs,
+                       method=method, options=options)
+        
+    if verbose:
+        print(res)
+
+    return res
 
 if __name__=='__main__':
 
@@ -39,10 +96,10 @@ if __name__=='__main__':
     def waveform(x, coeffs=[.3, .2, 2.]):
         return coeffs[2]*(np.sign(x-coeffs[0])+1.)*np.exp(-(x-coeffs[0])/coeffs[1])
 
-    import inspect
-    print(inspect.signature(waveform).coeffs)
-    # x = np.linspace(0, 1, 100)
-    # y = np.random.randn(100)+waveform(x)
+    x = np.linspace(0, 1, 100)
+    y = np.random.randn(100)+waveform(x, [0.5, .2, 4.])
 
-    # ge.plot(x, y)
-    # ge.show()
+    res = curve_fit(x, y, waveform)
+
+    ge.plot(x, Y=[y, waveform(x, res.x)])
+    ge.show()
